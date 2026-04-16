ed913da2a87dce02decfde449f8e708ca8a33b54
Md. Yousuf Khan <mdyousupkhan05@gmail.com>
2026-04-17 00:18:04 +0600

    Complite Nexcent Layout and Nav bar

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+tailwind.config ={
+    them:{
+        extend:{
+            colors:{
+                'brand-primary':
+                '#4CAF4F',
+                'brand-silver': '#F5F7FA',
+            },
+            fontFamily:{
+                inter:['inter','sans-serif'],
+            }
+        }
+    }
+}
